7bf58834759b198145e5dc350775236632ba2563
Mark Heydari <MHeydari@stephensons.ca>
2020-09-25 07:16:21 -0400

    Add project files.

---
+++ Live-Search-Filter-Ajax-JQuery/Modals/ApplicationDBContnextClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Live_Search_Filter_Ajax_JQuery.Modals
+{
+    public class ApplicationDBContnextClass
+    {
+    }
+}
---
+++ Live-Search-Filter-Ajax-JQuery/Modals/DispClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Live_Search_Filter_Ajax_JQuery.Modals
+{
+    public class DispClass
+    {
+        [Key]
+        public int DispId { get; set; }
+        public string Customer_No { get; set; }
+        public string SaleMan { get; set; }
+        public string NIC_Code { get; set; }
+        public string DisputeCode { get; set; }
+        public string Division { get; set; }
+    }
+}
